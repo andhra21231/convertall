f940a3a81024ba67fe3163c580b59d7fd0c2c04f
andhra21231 <andhrarifqi@icloud.com>
2022-03-25 10:53:41 +0700

    stupid me,

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/upload', uploads.single('file'), (req, res) => {
+app.post('/convert', uploads.single('file'), (req, res) => {
     if(req.file){
         console.log(req.file.path);
 
